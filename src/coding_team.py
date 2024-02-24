dd4e6f87e88d4e0f0d0c1b31ef480d4802d08322
rahul1990bhatia <rahul1990bhatia@users.noreply.github.com>
2024-02-24 01:44:32 +0000

    Update coding_team.py

--- src/coding_team.py
+++ src/coding_team.py
@@ -1,64 +1,43 @@
 import json
-from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
+from autogen import AssistantAgent, UserProxyAgent, config_list_from_json, GroupChat, GroupChatManager
 
 # This is actual coder who writes the python code.
-program_manager = AssistantAgent(
-        name = 'TechnicalProjectManager',
+pm = AssistantAgent(
+        name = 'Program_Manager',
         llm_config = {'config_list': config_list_from_json('src/config_files/openai.json')},
-        system_message = "You are a technical project manager. You break complex task into simple doable steps for coder and also reviews his code."
+        system_message = "You are a technical program manager. You can give create product ideas, review task, provide feedback and support the coder and tester."
         )
 
-user = UserProxyAgent(
-        name = 'user',
-        max_consecutive_auto_reply=0,
-        human_input_mode='NEVER',
-        code_execution_config={"work_dir": "coding", "use_docker": False},
+
+coder = AssistantAgent(
+        name='coder',
+        llm_config = {'config_list': config_list_from_json('src/config_files/openai.json')},
+        system_message = "you are python coder with 20 years of experience. You write extremely well python code. Your code is always well commented and easy to read. You also always create a readme file for code base."
         )
 
-def ask_program_manager(message):
-    user.initiate_chat(program_manager, message=message)
-    return user.last_message()['content']
-
-assistant = AssistantAgent(
-        name = 'assistant',
-        llm_config={
-            "temperature": 0,
-            "timeout": 600,
-            "cache_seed": 42,
-            "config_list": config_list_from_json('src/config_files/openai.json'),
-            "functions": [
-                {
-                    "name": "ask_program_manager",
-                    "description": "ask program manager to: 1. get a plan for finishing a task, 2. verify the execution result of the plan and potentially suggest new task.",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "message": {
-                                "type": "string",
-                                "description": "question to ask program manager. Make sure the question include enough context, such as the code and the execution result. The program manager does not know the conversation between you and the user, unless you share the conversation with the program manager.",
-                                },
-                        },
-                    "required": ["message"],
-                    },
-                }
-            ]
-            }
-)
+tester = AssistantAgent(
+        name='tester',
+        llm_config = {'config_list': config_list_from_json('src/config_files/openai.json')},
+        system_message = "whenever coder writes some code, can you please write pytest for same code base."
+        )
 
 # create a UserProxyAgent instance named "user_proxy"
-user_proxy = UserProxyAgent(
-        name="user_proxy",
-        human_input_mode="TERMINATE",
+user = UserProxyAgent(
+        name="user",
+        system_message = "A human admin who bring task and confirm the solution",
+        human_input_mode="ALWAYS",
         max_consecutive_auto_reply=10,
-    # is_termination_msg=lambda x: "content" in x and x["content"] is not None and x["content"].rstrip().endswith("TERMINATE"),
         code_execution_config={
-            "work_dir": "planning",
+            "last_n_messages": 2,
+            "work_dir": "myteam",
             "use_docker": False,
-        },  # Please set use_docker=True if docker is available to run the generated code. Using docker is safer than running the generated code directly.
-        function_map={"ask_project_manager": ask_program_manager},
-)
+        },
+    )
 
-user_proxy.initiate_chat(
-    assistant,
-    message="""Create a flask based web app for asking question and getting responses. Both questions and answers will be in str. Code should be written in python.""",
+groupchat = GroupChat(agents=[coder, user, pm, tester], messages=[], max_round=20)
+manager = GroupChatManager(groupchat=groupchat, llm_config = {'config_list': config_list_from_json('src/config_files/openai.json')})
+
+user.initiate_chat(
+    manager,
+    message="create a flask based app for using a LLM model name askGITA which can be deployed on internet. It has a field to read in a question from a user and it provides a response. In backend to get responsee it calls a function name : ragchain()"
 )
